3021bbc76f0192b490c93b29707d890e1c75391e
battlefeel1942 <40343790+battlefeel1942@users.noreply.github.com>
2024-04-19 12:41:27 +1200

    Update

--- startup.sh
+++ startup.sh
@@ -53,12 +53,6 @@ check_and_run_service() {
   fi
 }
 
-# Setup Ubuntu with SSH and Python
-check_and_run_service "ubuntu-ssh-python" "ubuntu-ssh-python" "ubuntu:latest" "2222:22" "" ""
-docker exec ubuntu-ssh-python apt-get update
-docker exec ubuntu-ssh-python apt-get install -y openssh-server python3 python3-pip
-docker exec ubuntu-ssh-python service ssh start
-
 # Setup Pi-hole with no password
 check_and_run_service "pihole" "pihole" "pihole/pihole:latest" "53:53/tcp;53:53/udp;67:67/udp;80:80/tcp;443:443/tcp" "TZ:'Pacific/Auckland';WEBPASSWORD=''" "./etc-pihole/:/etc/pihole/;./etc-dnsmasq.d/:/etc/dnsmasq.d/"
 
@@ -75,13 +69,13 @@ check_and_run_service "mumble" "mumble" "mumble-voip/mumble-server" "64738:64738
 check_and_run_service "deluge" "deluge" "linuxserver/deluge" "8112:8112/tcp;58846:58846/tcp;58946:58946/udp" "" "./deluge-config:/config"
 
 # Setup xTeVe
-check_and_run_service "xteve" "xteve" "tellytv/xteve" "34400:34400/tcp" "" "./xteve-config:/root/.xteve"
+# check_and_run_service "xteve" "xteve" "tellytv/xteve" "34400:34400/tcp" "" "./xteve-config:/root/.xteve"
 
 # Setup Home Assistant
 check_and_run_service "homeassistant" "homeassistant" "homeassistant/home-assistant" "8123:8123/tcp" "" "./homeassistant-config:/config"
 
 # Setup Freqtrade
-check_and_run_service "freqtrade" "freqtrade" "freqtrade/freqtrade:stable" "" "FT_CONFIGFILE:'/freqtrade/config.json';FT_STRATEGY:'SampleStrategy'" "./freqtrade-config:/freqtrade"
+# check_and_run_service "freqtrade" "freqtrade" "freqtrade/freqtrade:stable" "" "FT_CONFIGFILE:'/freqtrade/config.json';FT_STRATEGY:'SampleStrategy'" "./freqtrade-config:/freqtrade"
 
 # Setup Ubuntu Desktop with kasmweb/desktop as web-desktop
 check_and_run_service "web-desktop" "web-desktop" "kasmweb/desktop" "6901:6901" "" ""
