373eb1e8d47f317a677a29e318423772a36cff54
battlefeel1942 <40343790+battlefeel1942@users.noreply.github.com>
2024-04-19 22:00:07 +1200

    Updates

--- startup.sh
+++ startup.sh
@@ -7,44 +7,36 @@ PUSHBULLET_TOKEN_FILE="$CONFIG_DIR/pushbullet_token"
 SAMBA_USER_FILE="$CONFIG_DIR/samba_username"
 SAMBA_PASS_FILE="$CONFIG_DIR/samba_password"
 
-# Check if the Pushbullet token file exists and read from it, if not, prompt for it and save
-if [ -f "$PUSHBULLET_TOKEN_FILE" ]; then
-    PUSHBULLET_TOKEN=$(cat "$PUSHBULLET_TOKEN_FILE")
-else
-    read -p "Enter your Pushbullet access token: " PUSHBULLET_TOKEN
-    echo
-    echo "$PUSHBULLET_TOKEN" > "$PUSHBULLET_TOKEN_FILE"
-    chmod 600 "$PUSHBULLET_TOKEN_FILE"
-fi
+# Function to check and prompt for credentials
+function check_and_prompt_for_credential() {
+    local credential_file=$1
+    local credential_name=$2
+    local var_name=$3
+    local credential_value
+    if [ -f "$credential_file" ]; then
+        credential_value=$(cat "$credential_file")
+    else
+        read -p "Enter your $credential_name: " credential_value
+        echo $credential_value > "$credential_file"
+        chmod 600 "$credential_file"
+    fi
+    eval $var_name="'$credential_value'"
+}
+
+# Prompt for Pushbullet token, Samba username and password
+check_and_prompt_for_credential "$PUSHBULLET_TOKEN_FILE" "Pushbullet access token" PUSHBULLET_TOKEN
+check_and_prompt_for_credential "$SAMBA_USER_FILE" "Samba username" USERNAME
+check_and_prompt_for_credential "$SAMBA_PASS_FILE" "Samba password" PASSWORD
 
 # Function to send notification via Pushbullet
 send_pushbullet_notification() {
     local title="$1"
     local message="$2"
     local token="$PUSHBULLET_TOKEN"
-
     curl -u "$token:" -X POST https://api.pushbullet.com/v2/pushes \
-        --header 'Content-Type: application/json' \
-        --data-binary "{\"type\": \"note\", \"title\": \"$title\", \"body\": \"$message\"}"
+         --header 'Content-Type: application/json' \
+         --data-binary "{\"type\": \"note\", \"title\": \"$title\", \"body\": \"$message\"}"
 }
-
-# Check and prompt for Samba username and password
-if [ -f "$SAMBA_USER_FILE" ]; then
-    USERNAME=$(cat "$SAMBA_USER_FILE")
-else
-    read -p "Enter your Samba username: " USERNAME
-    echo "$USERNAME" > "$SAMBA_USER_FILE"
-    chmod 600 "$SAMBA_USER_FILE"
-fi
-
-if [ -f "$SAMBA_PASS_FILE" ]; then
-    PASSWORD=$(cat "$SAMBA_PASS_FILE")
-else
-    read -p "Enter your Samba password: " PASSWORD
-    echo
-    echo "$PASSWORD" > "$SAMBA_PASS_FILE"
-    chmod 600 "$SAMBA_PASS_FILE"
-fi
 
 # Add script to crontab to run at reboot and daily at 4 AM
 SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
@@ -58,20 +50,17 @@ sudo systemctl enable ssh
 sudo systemctl start ssh
 
 # Check if Docker is installed and install if it isn't
-if ! [ -x "$(command -v docker)" ]; then
+if ! command -v docker > /dev/null; then
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
   mkdir -p ~/docker-services
   touch ~/docker-services/docker-compose.yml
-  cat <<EOF > ~/docker-services/docker-compose.yml
-version: '3'
-services:
-EOF
-  # sudo systemctl reboot
+  echo "version: '3'" > ~/docker-services/docker-compose.yml
+  echo "services:" >> ~/docker-services/docker-compose.yml
 fi
 
 # Check if Docker Compose is installed, if not install it
-if ! [ -x "$(command -v docker-compose)" ]; then
+if ! command -v docker-compose > /dev/null; then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
@@ -100,34 +89,21 @@ EOF
 check_and_run_service() {
     if ! docker ps | grep -q $1; then
         add_service_to_docker_compose "$@"
-        cd ~/docker-services
+        cd ~/docker-services || exit
         docker-compose up -d
         send_pushbullet_notification "Docker Update" "$1 container has been updated or restarted"
-        # sudo systemctl reboot
     fi
 }
 
-# Setup Samba Network Share with secure credentials
-USER_ID="1000"
-GROUP_ID="1000"
-SHARE_NAME="share"
-
-# Define the share directory
+# Define and check the share directory permissions
 SHARE_DIR="$HOME/share"
-
-# Check if the directory exists
-if [ ! -d "$SHARE_DIR" ]; then
-    # If it doesn't exist, create it and set permissions
+if [ ! -d "$SHARE_DIR" ] || [ "$(stat -c '%a' "$SHARE_DIR")" != "777" ]; then
     mkdir -p "$SHARE_DIR"
     chmod 777 "$SHARE_DIR"
-else
-    # If it exists, check the current permissions
-    current_perms=$(stat -c "%a" "$SHARE_DIR")
-    if [ "$current_perms" != "777" ]; then
-        # If permissions are not 777, change them
-        chmod 777 "$SHARE_DIR"
-    fi
 fi
+
+
+
 check_and_run_service "samba" "samba" "dperson/samba" "139:139/tcp;445:445/tcp" "USER:'$USERNAME;$PASSWORD;$USER_ID;$GROUP_ID;$SHARE_NAME'" "${SHARE_DIR}:/share:rw"
 
 # Setup Pi-hole with no password
